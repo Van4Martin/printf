15168c0ca6fca0a0f226516e04fb4faac4ca8d85
NnannaOgbu <nnannaogbuwa@gmail.com>
2023-04-24 16:49:41 -0700

    Function to print an interget script

---
+++ printint.c
@@ -0,0 +1,35 @@
+include "main.h"
+
+/**
+ * printint - print interget to standard output
+ * @x: integer to be printed
+ * Authors: Godswill && Martin
+ * Return: count
+*/
+
+
+int printint(int x)
+{
+	int count = 0;
+
+	if (x < 0)
+	{
+		_putchar('-');
+		count++;
+		x = -x;
+	}
+
+	if (x / 10)
+	{
+		printint(x / 10);
+		count++;
+	}
+
+	_putchar(x % 10 + '0');
+	count++;
+
+	return (count);
+
+
+}
+
