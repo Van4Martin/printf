e510ebd4e0e73ce9233693e45fcfda80dc159fb1
Martin <van4martin@gmail.com>
2023-04-24 08:03:12 -0700

    "good"

--- printf.c
+++ printf.c
@@ -1,54 +1,51 @@
 #include "main.h"
 /**
  * _printf - prints to the stdio
- * It considers format specifiers 
  * @format: Input specifiers
- *
- * Authors - Martin Dotsey & Godswill Ogbuwa
  * Return: count
  */
 int _printf(const char *format, ...)
 {
-    va_list arg_v;
-    va_start(arg_v, format);
+	va_list arg_v;
+	int count = 0;
+	char c;
 
-    int count = 0;
-    char c;
+	va_start(arg_v, format);
+	while ((c =  *(format++)))
+	{
+		if (c == '%')
+	{
+		c = *(format++);
+		switch (c)
+		{
+			case 'c':
+				{
+				int  arg =  va_arg(arg_v, int);
 
-    while ((c = *(format++))) {
-        if (c == '%') {
-            c = *(format++);
-            switch (c) {
-                case 'c': {
-                    char arg = (char) va_arg(arg_v, int);
-                    _putchar(arg);
-                    count++;
-                    break;
-                }
-                case 's': {
-                    char* arg = va_arg(arg_v, char*);
-                    while (*arg) {
-                        _putchar(*(arg++));
-                        count++;
-                    }
-                    break;
-                }
-                case '%': {
-                    _putchar('%');
-                    count++;
-                    break;
-                }
-                default:
-                    break;
-            }
-        } else {
-            _putchar(c);
-            count++;
-        }
-    }
+				count += _putchar((char)arg);
+				}
+				break;
+			case 's':
+				{
+				char *str = va_arg(arg_v, char*);
 
-    va_end(arg_v);
-
-    return count;
+				while (*str)
+				{
+					count =+ _putchar(*(str++));
+				}
+				}
+				break;
+			case '%':
+				count += _putchar('%');
+				break;
+		}
+	}
+	else
+	{
+		_putchar(c);
+		count++;
+	}
+	}
+	va_end(arg_v);
+	return (count);
 }
-
